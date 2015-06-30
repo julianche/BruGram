51582416969e0ae6b131de9a275aa67ac4b56dfb
julianche <julian.chernyk@gmail.com>
2015-06-30 12:23:28 +0200

    added photo model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150630095903) do
+ActiveRecord::Schema.define(version: 20150630102239) do
+
+  create_table "photos", force: :cascade do |t|
+    t.boolean  "public"
+    t.text     "caption"
+    t.string   "image"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "photos", ["user_id"], name: "index_photos_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
